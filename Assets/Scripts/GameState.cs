5dd9c94006de50af5f044f5b6c3cd1514fda1755
Jakub Bentkowski <bentkowski.jakub@gmail.com>
2018-04-21 13:30:42 +0200

    Common code and Rider

---
+++ Assets/Scripts/GameState.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class GameState : MonoBehaviour
+{
+    [System.Serializable]
+    public struct TheEvent
+    {
+        [SerializeField]
+        public float Delay;
+
+        [SerializeField]
+        public UnityEvent OnPreEvent;
+        
+        [SerializeField]
+        public UnityEvent OnPostEvent;
+        
+        public void Execute(MonoBehaviour context)
+        {
+            context.StartCoroutine(CoExecute());
+        }
+
+        IEnumerator CoExecute()
+        {
+            OnPreEvent.Invoke();
+            yield return new WaitForSeconds(Delay);
+            OnPostEvent.Invoke();
+        }
+    }
+
+    public TheEvent OnBeginEvent;
+    public TheEvent OnEndEvent;
+
+    public void Begin()
+    {
+        OnBeginEvent.Execute(this);
+        OnBegin();
+    }
+
+    protected virtual void OnBegin()
+    { }
+
+    public void End()
+    {
+        OnEndEvent.Execute(this);
+        OnEnd();
+    }
+    
+    protected virtual void OnEnd()
+    { }
+
+    public virtual void OnUpdate()
+    { }
+    
+    public virtual void OnLateUpdate()
+    { }
+    
+    public virtual void OnFixedUpdate()
+    { }
+}
---
+++ Assets/Scripts/MainGame.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class MainGame : MonoBehaviour
+{
+    public GameState StartState;
+    
+    public GameState CurrentState { get; set; }
+    
+    private GameState NextState { get; set; }
+
+    private List<GameState> AllStates;
+
+    void Start()
+    {
+        AllStates.AddRange(FindObjectsOfType<GameState>());
+        
+        SwitchState(StartState);
+    }
+
+    public void ScheduleState<T>()
+    {
+        ScheduleState(AllStates.FirstOrDefault(s => s is T));
+    }
+
+    public void ScheduleState(GameState state)
+    {
+        NextState = state;
+    }
+
+    public void SwitchState<T>()
+    {
+        SwitchState(AllStates.FirstOrDefault(s => s is T));
+    }
+    
+    public void SwitchState(GameState state)
+    {
+        if (CurrentState)
+            CurrentState.End();
+
+        CurrentState = state;
+        
+        if (CurrentState)
+            CurrentState.Begin();
+    }
+
+    void Update()
+    {
+        if (CurrentState)
+            CurrentState.OnUpdate();
+    }
+
+    void FixedUpdate()
+    {
+        if (CurrentState)
+            CurrentState.OnFixedUpdate();
+    }
+
+    void LateUpdate()
+    {
+        if (NextState)
+        {
+            SwitchState(NextState);
+            NextState = null;
+        }
+
+        if (CurrentState)
+            CurrentState.OnLateUpdate();
+    }
+}
